d6c404513c634d80b5a9c46587c4b455b927f3bc
itskarudo <itskarudo@protonmail.com>
2025-02-26 14:57:24 +0100

    Surround bytes macro parameters with parens

--- lib/include/bytes.h
+++ lib/include/bytes.h
@@ -21,36 +21,36 @@ const char *b_s(const bytes_t *);
 }
 #endif
 
-#define b(v) (b_new_v((uint8_t *)v, sizeof(v) - 1))
-#define b_d(v) (v->_data)
-#define b_at(v, i) (v->_data[i])
-#define b_len(v) (v->_len)
+#define b(v) (b_new_v((uint8_t *)(v), sizeof(v) - 1))
+#define b_d(v) ((v)->_data)
+#define b_at(v, i) ((v)->_data[i])
+#define b_len(v) ((v)->_len)
 
 #define p8(v)                                                                  \
   __extension__({                                                              \
-    uint8_t _tmp = v;                                                          \
+    uint8_t _tmp = (v);                                                        \
     b_new_v(&_tmp, 1);                                                         \
   })
 
 #define p16(v)                                                                 \
   __extension__({                                                              \
-    uint16_t _tmp = v;                                                         \
+    uint16_t _tmp = (v);                                                       \
     b_new_v((uint8_t *)&_tmp, 2);                                              \
   })
 
 #define p32(v)                                                                 \
   __extension__({                                                              \
-    uint32_t _tmp = v;                                                         \
+    uint32_t _tmp = (v);                                                       \
     b_new_v((uint8_t *)&_tmp, 4);                                              \
   })
 
 #define p64(v)                                                                 \
   __extension__({                                                              \
-    uint64_t _tmp = v;                                                         \
+    uint64_t _tmp = (v);                                                       \
     b_new_v((uint8_t *)&_tmp, 8);                                              \
   })
 
-#define u8(v) (*(uint8_t *)v->_data)
-#define u16(v) (*(uint16_t *)v->_data)
-#define u32(v) (*(uint32_t *)v->_data)
-#define u64(v) (*(uint64_t *)v->_data)
+#define u8(v) (*(uint8_t *)(v)->_data)
+#define u16(v) (*(uint16_t *)(v)->_data)
+#define u32(v) (*(uint32_t *)(v)->_data)
+#define u64(v) (*(uint64_t *)(v)->_data)
