c1483a66680ff14cd7642de1cd0656ac4c4ca1b3
itskarudo <itskarudo@protonmail.com>
2025-07-22 15:39:40 +0100

    cyclic: add c++ header guards to cyclic.h

--- lib/include/cyclic.h
+++ lib/include/cyclic.h
@@ -2,5 +2,13 @@
 
 #include "bytes.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 bytes_t *de_bruijn(bytes_t *alphabet, size_t n, size_t length);
 bytes_t *cyclic(size_t length);
+
+#ifdef __cplusplus
+}
+#endif
